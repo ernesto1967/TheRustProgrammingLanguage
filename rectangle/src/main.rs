793ac276bd4465fa0e2f70d95c2fac17af214ed0
Ernesto <ernesto.1967@web.de>
2020-08-30 23:47:56 +0200

    rectangle: Add some methods to Rectangle

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -28,16 +28,49 @@ fn area_tupel(dimensions: (u32, u32)) -> u32 {
 }
 
 // Define the dimensions of the rectangle using a struct
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
 }
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+impl Rectangle {
+    fn square(size : u32) -> Rectangle {
+        Rectangle {
+            width : size,
+            height : size,
+        }
+    }
+}
 fn rectangle_struct() {
-    let rect = Rectangle {
+    let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
-    println!("The area of rect_struct is {}", area_struct(&rect));
+
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("rect_struct is {:?}", rect1);
+    println!("The area of rect_struct is {}", area_struct(&rect1));
+    println!("The area of rect_struct is {}", rect1.area());
+    println!("rect1 can hold rect2? {}", rect1.can_hold(&rect2));
+    println!("rect1 can hold rect3? {}", rect1.can_hold(&rect3));
+    println!("A square of size 10 is {:?}", Rectangle::square(10));
 }
 
 fn area_struct(rectangle: &Rectangle) -> u32 {
